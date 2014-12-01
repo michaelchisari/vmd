9bf11e7c035cb5f607d719c7352a487e336ceffb
Michael Chisari <michael@chisari.us>
2014-12-01 00:36:57 -0800

    Removed old html data

--- src/js/07.vmd.toolbar.js
+++ src/js/07.vmd.toolbar.js
@@ -1,26 +1,6 @@
 /*  Build Toolbar */
 $vmd.buildToolbar = function() {
 
-    var vmdOnOff = '';
-        vmdOnOff += '  <div id="vmd-logo" class="ui mini label">VMD</div>';
-        vmdOnOff += '  <div id="vmd-on-off" class="ui mini circular toggle button">Off</div>';
-
-    var vmdMenu = '';
-        vmdMenu += '  <div class="left menu">';
-        vmdMenu += '  <div class="ui mini disabled button ui-widget-handle">|||</div>';
-        vmdMenu +=    vmdOnOff;
-        vmdMenu += '  </div>';
-        vmdMenu += '  <div id="vmd-ui-buttons" class="mini attached ui buttons">';
-        vmdMenu += '  </div>';
-        vmdMenu += '  <div class="right menu">';
-        vmdMenu += '    <div class="ui mini buttons">';
-        vmdMenu += '        <div class="vmd-play-all ui positive button">1 <i class="play icon"></i></div>';
-        vmdMenu += '        <div class="or"></div>';
-        vmdMenu += '        <div class="vmd-stop-all ui negative button">2 <i class="pause icon"></i></div>';
-        vmdMenu += '    </div>';
-        vmdMenu += '  </div>';
-        vmdMenu += '  </div>';
-
     var vmdMenu = '';
         vmdMenu += '<div class="vmd-toolbar ui fitted segment">';
         vmdMenu += '    <div class="ui two column grid">';
