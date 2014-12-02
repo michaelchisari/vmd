414552c61bada31d62668410ac890b8ce1d0557a
Michael Chisari <michael@chisari.us>
2014-12-01 22:20:39 -0800

    Code cleanup.

--- src/js/07.vmd.toolbar.js
+++ src/js/07.vmd.toolbar.js
@@ -1,24 +1,24 @@
 /*  Build Toolbar */
-VMD.buildToolbar = function() {
+VMD.buildToolbar = function vmdBuildToolbar() {
 
-    var _menu = '';
-        _menu += '<div class="vmd-toolbar ui fitted segment">';
-        _menu += '    <div class="ui two column grid">';
-        _menu += '        <div class="left floated column">';
-        _menu += '            <div class="ui mini label vmd-toolbar-handle">|||</div>';
-        _menu += '            <div id="vmd-logo" class="ui mini label">VMD</div>';
-        _menu += '            <div id="vmd-on-off" class="ui mini circular toggle button">Off</div>';
-        _menu += '            <div id="vmd-ui-buttons" class="mini attached ui buttons"></div>';
-        _menu += '        </div>';
-        _menu += '        <div class="right floated right aligned column">';
-        _menu += '            <div class="ui mini buttons">';
-        _menu += '                <div class="vmd-play-all ui positive button">1 <i class="play icon"></i></div>';
-        _menu += '                <div class="or"></div>';
-        _menu += '                <div class="vmd-play-all ui negative button">2 <i class="pause icon"></i></div>';
-        _menu += '            </div>';
-        _menu += '        </div>';
-        _menu += '    </div>';
-        _menu += '</div>';
+    var _toolbar = '' +
+                   '<div class="vmd-toolbar ui fitted segment">' +
+                   '    <div class="ui two column grid">' +
+                   '        <div class="left floated column">' +
+                   '            <div class="ui mini label vmd-toolbar-handle">|||</div>' +
+                   '            <div id="vmd-logo" class="ui mini label">VMD</div>' +
+                   '            <div id="vmd-on-off" class="ui mini circular toggle button">Off</div>' +
+                   '            <div id="vmd-ui-buttons" class="mini attached ui buttons"></div>' +
+                   '        </div>' +
+                   '        <div class="right floated right aligned column">' +
+                   '            <div class="ui mini buttons">' +
+                   '                <div class="vmd-play-all ui positive button">1 <i class="play icon"></i></div>' +
+                   '                <div class="or"></div>' +
+                   '                <div class="vmd-play-all ui negative button">2 <i class="pause icon"></i></div>' +
+                   '            </div>' +
+                   '        </div>' +
+                   '    </div>' +
+                   '</div>';
 
-    return _menu;
+    return _toolbar;
 }
--- src/js/08.vmd.form.js
+++ src/js/08.vmd.form.js
@@ -1,54 +1,54 @@
 /*  Build Form */
-VMD.buildForm = function () {
+VMD.buildForm = function vmdBuildForm() {
 
     /* Input form for animations */
-    var _form = '';
-        _form += '<div class="vmd-form">';
-        _form += '    <div class="vmd-form-handle ui top attached black block header segment">';
-        _form += '        <div class="ui three column grid">';
-        _form += '            <div class="vmd-identifier ui column">.vmd-identifier</div>';
-        _form += '            <div class="ui center aligned column">';
-        _form += '                <div class="vmd-alphabet ui mini circular button">A</div>';
-        _form += '            </div>';
-        _form += '            <div class="ui right aligned column">';
-        _form += '                <i class="vmd-repeat-icon lightgray small repeat icon" data-content="Repeat" data-variation="basic small" data-position="right center"></i>';
-        _form += '            </div>';
-        _form += '        </div>';
-        _form += '    </div>';
-        _form += '    <div class="ui fluid attached form segment">';
-        _form += '      <div class="field">';
-        _form += '          <label>Transition</label>';
-        _form += '          <div class="ui fluid action input">';
-        _form += '            <input type="text" name="transition" placeholder="example: { translateX: [0, -1000] }">';
-        _form += '            <div class="vmd-play ui button"><i class="play icon"></i></div>';
-        _form += '          </div>';
-        _form += '      </div>';
-        _form += '      <div class="four fields">';
-        _form += '        <div class="field">';
-        _form += '            <label>Duration: <b><span class="vmd-duration-amount">' + VMD._durationDefault + '</span>ms</b></label>';
-        _form += '            <input type="range" name="duration" min="100" max="4000" value="' + VMD._durationDefault + '" />';
-        _form += '        </div>';
-        _form += '        <div class="field">';
-        _form += '            <label>Delay: <b><span class="vmd-delay-amount">' + VMD._delayDefault + '</span>ms</b></label>';
-        _form += '            <input type="range" name="delay" min="0" max="2000" value="' + VMD._delayDefault + '" />';
-        _form += '        </div>';
-        _form += '        <div class="field">';
-        _form += '            <select name="easing" class="vmd-easing-dropdown ui selection dropdown" id="easing">';
-        _form += '            </select>';
-        _form += '        </div>';
-        _form += '        <div class="field">';
-        _form += '          <div class="vmd-easing-data ui fluid input">';
-        _form += '            <input type="text" name="easing-data" placeholder="eg, [50,20]" value="[ ]">';
-        _form += '          </div>';
-        _form += '        </div>';
-        _form += '      </div>';
-        _form += '    </div>';
-        _form += '    <div class="ui bottom attached black block header segment">';
-        _form += '        <div class="ui two column grid">';
-        _form += '            <div class="ui vmd-identifier gray right aligned right floated column">.vmd-identifier</div>';
-        _form += '        </div>';
-        _form += '    </div>';
-        _form += '</div>';
+    var _form = '' +
+                '<div class="vmd-form">' +
+                '    <div class="vmd-form-handle ui top attached black block header segment">' +
+                '        <div class="ui three column grid">' +
+                '            <div class="vmd-identifier ui column">.vmd-identifier</div>' +
+                '            <div class="ui center aligned column">' +
+                '                <div class="vmd-alphabet ui mini circular button">A</div>' +
+                '            </div>' +
+                '            <div class="ui right aligned column">' +
+                '                <i class="vmd-repeat-icon lightgray small repeat icon" data-content="Repeat" data-variation="basic small" data-position="right center"></i>' +
+                '            </div>' +
+                '        </div>' +
+                '    </div>' +
+                '    <div class="ui fluid attached form segment">' +
+                '      <div class="field">' +
+                '          <label>Transition</label>' +
+                '          <div class="ui fluid action input">' +
+                '            <input type="text" name="transition" placeholder="example: { translateX: [0, -1000] }">' +
+                '            <div class="vmd-play ui button"><i class="play icon"></i></div>' +
+                '          </div>' +
+                '      </div>' +
+                '      <div class="four fields">' +
+                '        <div class="field">' +
+                '            <label>Duration: <b><span class="vmd-duration-amount">' + VMD._durationDefault + '</span>ms</b></label>' +
+                '            <input type="range" name="duration" min="100" max="4000" value="' + VMD._durationDefault + '" />' +
+                '        </div>' +
+                '        <div class="field">' +
+                '            <label>Delay: <b><span class="vmd-delay-amount">' + VMD._delayDefault + '</span>ms</b></label>' +
+                '            <input type="range" name="delay" min="0" max="2000" value="' + VMD._delayDefault + '" />' +
+                '        </div>' +
+                '        <div class="field">' +
+                '            <select name="easing" class="vmd-easing-dropdown ui selection dropdown" id="easing">' +
+                '            </select>' +
+                '        </div>' +
+                '        <div class="field">' +
+                '          <div class="vmd-easing-data ui fluid input">' +
+                '            <input type="text" name="easing-data" placeholder="eg, [50,20]" value="[ ]">' +
+                '          </div>' +
+                '        </div>' +
+                '      </div>' +
+                '    </div>' +
+                '    <div class="ui bottom attached black block header segment">' +
+                '        <div class="ui two column grid">' +
+                '            <div class="ui vmd-identifier gray right aligned right floated column">.vmd-identifier</div>' +
+                '        </div>' +
+                '    </div>' +
+                '</div>';
 
     return (_form);
 }
